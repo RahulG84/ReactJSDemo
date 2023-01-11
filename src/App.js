eaf438ba8668356016e53dc351d91dee95c68904
Rahul Girgal <rahul4girgal@gmail.com>
2023-01-11 16:46:23 +0530

    Added: Updated Code..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+//
+import React, { useState } from "react";
+import "../src/Button.css";
+import background1 from "../src/assets/pexels-photo-129731.jpeg";
+// import background from "../src/assets/pexels-pixabay-531880.jpg";
+
+const Mydata = [
+  {
+    id: 1,
+    name: "Rahul"
+  },
+  {
+    id: 2,
+    name: "Rohit"
+  }
+];
 
 function App() {
+  const [count, setcount] = useState(3);
+  const [text, setText] = useState(100);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="MainView"
+      style={{
+        backgroundImage: `url(${background1})`
+      }}
+    >
+      <div>
+        <text className="textStyle">{count}</text>
+        <br/>
+        <text className="textStyle">{text}</text>
+        <br/>
+        <button className="Button" onClick={() => setText(text + 200)}>
+          Click
+        </button>
+        <br/>
+        <button className="Button" onClick={() => setText(0)}>
+          Rest
+        </button>
+      </div>
+      <div>
+        <button id="Button" onClick={() => setcount(count + 1)}>
+          Click
+        </button>
+        <ul>
+          {Mydata.map((item) => (
+            <div>
+              <p key={item.id}>{item.name}</p>
+              <text>{item.id}</text>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p> p--> Hello World</p>
+        <h1>h1 --> Hello World</h1>
+        <h2>h2 --> Hello World</h2>
+        <h3>h3 --> Hello World</h3>
+        <h4>h4 --> Hello World</h4>
+        <span>span --> Hello World</span>
+        <span>span --> Hello World</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+
